0e382d11f6fbc25439162c61296b21089b6e05fe
lakhwinder <lakhwindersinghx2@gmail.com>
2024-01-04 18:24:14 -0500

    added complete,enumerate for show

--- to_do.py
+++ to_do.py
@@ -6,17 +6,25 @@ while True:
         case "add":
             task=input("add " )
             to_do_list.append(task)
+
         case "show":
-            for item in to_do_list:
-                print(item.capitalize())
+            for index,item in enumerate(to_do_list):
+                print(f"{index+1}-{item.capitalize()}")
+
         case "edit":
-            number=int(input("Which item number do you want to replace?"))
-            number=number-1
+            edit_number=int(input("Which task number do you want to replace?"))
+            edit_number=edit_number-1
             new_todo=input("Please add new task: ")
-            to_do_list[number]=new_todo
+            to_do_list[edit_number]=new_todo
+
+        case "complete":
+            complete_number=int(input("Which task number have you completed?"))
+            complete_number=complete_number-1
+            completed_task=to_do_list.pop(complete_number)
 
         case "exit":
             break
+
         case wrong_prompt:
             print("Invalid prompt")
 
