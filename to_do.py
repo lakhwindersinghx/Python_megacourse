158ba34108c867d704e209dec1248e20a91aafd4
lakhwinder <lakhwindersinghx2@gmail.com>
2024-01-04 16:50:24 -0500

    added edit

--- to_do.py
+++ to_do.py
@@ -9,9 +9,12 @@ while True:
         case "show":
             for item in to_do_list:
                 print(item.capitalize())
-    # elif ask=="complete":
-    #     completed_task=
-    #     to_do_list.pop()
+        case "edit":
+            number=int(input("Which item number do you want to replace?"))
+            number=number-1
+            new_todo=input("Please add new task: ")
+            to_do_list[number]=new_todo
+
         case "exit":
             break
         case wrong_prompt:
