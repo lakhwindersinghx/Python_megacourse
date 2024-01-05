e555a4455d364528e6abfcbf892724342d0613b2
lakhwinder <lakhwindersinghx2@gmail.com>
2024-01-05 10:38:42 -0500

    edit,complete now make changes to the text file, did some formatting too

--- to_do.py
+++ to_do.py
@@ -1,39 +1,45 @@
 to_do_list=[]
-print(to_do_list)
 while True:
-    ask = input("Type add,show,edit,complete or exit: ")
+    ask = input("Type add, show, edit, complete or exit: ")
+
     match ask:
         case "add":
-            task=input("add " )+"\n"
-            to_do_list.append(task)
-            file=open('to_do_list.txt','a') #file opened in append mode 'a' preserves old data aka doesnt overwrite
-            file.writelines(to_do_list)
-            file.close()
+            task = input("Add: ") + "\n"
+            task_title = task.title()
+            to_do_list.append(task_title)
+            with open('to_do_list.txt', 'w') as file:
+                file.writelines(to_do_list)
 
         case "show":
-            file = open('to_do_list.txt', 'r')
-            to_do_list=file.readlines()
-
-            for index,item in enumerate(to_do_list):
-                print(f"{index+1}-{item.capitalize()}")
-
+            with open('to_do_list.txt', 'r') as file:
+                to_do_list = file.readlines()
+            for index, item in enumerate(to_do_list):
+                item = item.strip("\n")
+                print(f"{index + 1}-{item.capitalize()}")
 
         case "edit":
-            edit_number=int(input("Which task number do you want to replace?"))
-            edit_number=edit_number-1
-            new_todo=input("Please add new task: ")
-            to_do_list[edit_number]=new_todo
+            edit_number = int(input("Which task number do you want to replace?"))
+            edit_number = edit_number - 1
+            with open('to_do_list.txt', 'r') as file:
+                to_do_list = file.readlines()
+            new_todo = input("Please add new task: ") + "\n"
+            to_do_list[edit_number] = new_todo
+            with open('to_do_list.txt', 'w') as file:
+                file.writelines(to_do_list)
 
         case "complete":
-            complete_number=int(input("Which task number have you completed?"))
-            complete_number=complete_number-1
-            completed_task=to_do_list.pop(complete_number)
+            complete_number = int(input("Which task number have you completed? "))
+            complete_number = complete_number - 1
 
+            with open('to_do_list.txt', 'r') as file:
+                to_do_list = file.readlines()
+                removed_task=to_do_list[complete_number]
+            print(f"Task number:{to_do_list.index(removed_task)}-{removed_task}has been removed from the list")
+            completed_task = to_do_list.pop(complete_number)
+            with open('to_do_list.txt', 'w') as file:
+                file.writelines(to_do_list)
         case "exit":
             break
 
-        case wrong_prompt:
+        case _:
             print("Invalid prompt")
-
-
-
